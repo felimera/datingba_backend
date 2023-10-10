38ebebd7eb3a8ffb0c752381ae4fb8fb22c5c5c2
andres.mera <feli041089@gmail.com>
2023-10-10 08:01:30 -0500

    Creaccion del Proyecto Backend

---
+++ src/main/java/com/proyect/datingba_backend/DatingbaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.datingba_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingbaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingbaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyect/datingba_backend/DatingbaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.datingba_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingbaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
